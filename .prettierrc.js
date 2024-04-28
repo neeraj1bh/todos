aa8f9d04fed16a3f95a8516a0f465c8a372e2082
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-04-28 12:15:22 +0530

    feat: init setup

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 100,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+};
---
+++ app.config.ts
@@ -0,0 +1,13 @@
+import 'ts-node/register'; // Add this to import TypeScript files
+import { ExpoConfig } from 'expo/config';
+
+// In SDK 46 and lower, use the following import instead:
+// import { ExpoConfig } from '@expo/config-types';
+
+const config: ExpoConfig = {
+  name: 'todos',
+  slug: 'todos',
+  scheme: 'todos',
+};
+
+export default config;
